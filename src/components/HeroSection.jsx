0ba4997365e93ecd1f40df2e9fc69042f293ef48
Boost <boostbm@gmail.com>
2022-08-27 20:14:15 -0700

    Updated CSS

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 import "../components/HeroSection.css"
+import Fade from "react-reveal/Fade"
 
 function HeroSection() {
   return (
-    <div>
-      <h1>DAVID YU</h1>
-      <p>WEB DEVELOPER / B-BOY / JIU JITSU PRACTICTIONER</p>
+    <div className="container">
+      <Fade>
+        <h1>DAVID YU</h1>
+        <p>WEB DEVELOPER / B-BOY / JIU JITSU PRACTICTIONER</p>
+      </Fade>
     </div>
   )
 }
